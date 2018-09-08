78c8446cdca64b07beab12a76df28452151a0709
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-08 18:58:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  def initialize(name:, grade:, id: nil)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
     @id = id
@@ -63,7 +63,7 @@ def update
 
 def self.new_from_db(row)
    new = self.new(id = row[0], name = row[1], grade = row[2])
-  new
+   new
     
 
 end
