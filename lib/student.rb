9e4e52c0ac6ccf7dbcd47d3cfa8303568bf18d88
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-08 18:50:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,12 +62,11 @@ def update
  end
 
 def self.new_from_db(row)
-   new = self.new(id, name, grade)
-  new.id = row[0]
-  new.name =  row[1]
-  new.grade = row[2]
-  # binding.pry
-    new.save
+  self.new.tap do |s|	
+      s.id = row[0]	
+      s.name =  row[1]	
+      s.grade = row[2]	
+    end
     
 
 end
